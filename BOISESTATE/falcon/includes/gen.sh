ef10f2c1fde691c416d0c123918b168430bb4642
Frank Willmore <frankwillmore@gmail.com>
2022-07-29 10:58:48 -0600

    added gromacs build, and netcdf stack in prep for wrf. also, explicit includes for hdf5 and netcdf

--- BOISESTATE/falcon/includes/gen.sh
+++ BOISESTATE/falcon/includes/gen.sh
@@ -1,4 +1,17 @@
-echo "packages:" > system_packages2.yaml
-echo "" >> system_packages2.yaml
-spack find -X --format "QZ{name}:newline    version:newline    - {version}newline    externals:newline    - spec: {name}@{version}newline      prefix: {prefix}newline      buildable: Falsenewline" |sed -e "s/newline/\n/g" | sed -e "s/QZ/  /" >> system_packages2.yaml
-echo "Generated system_packages2.yaml."
+#!/bin/bash
+
+packages_file=$1
+
+echo "Generating as $1"
+
+#echo "packages:" > system_packages2.yaml
+#echo "" >> system_packages2.yaml
+#spack find -X --format "QZ{name}:newline    version:newline    - {version}newline    externals:newline    - spec: {name}@{version}newline      prefix: {prefix}newline      buildable: Falsenewline" |sed -e "s/newline/\n/g" | sed -e "s/QZ/  /" >> system_packages2.yaml
+#echo "Generated system_packages2.yaml."
+
+
+echo "packages:" > ${packages_file}
+echo "" >> ${packages_file}
+#spack find -x --format "QZ{name}:newline    version:newline    - {version}newline    externals:newline    - spec: {name}@{version}newline      prefix: {prefix}newline      buildable: Falsenewline" |sed -e "s/newline/\n/g" | sed -e "s/QZ/  /" >> ${packages_file}
+spack find -x --format "QZ{name}:newline    version:newline    - {version}newline    externals:newline    - spec: {name}@{version}%{compiler}^mpich newline      prefix: {prefix}newline      buildable: Falsenewline" |sed -e "s/newline/\n/g" | sed -e "s/QZ/  /" >> ${packages_file}
+echo "Generated ${packages_file}."
